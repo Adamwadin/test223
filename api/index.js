6851e13d224e3e8b25efe7d18452bcf7fcefb12b
Adamwadin <adamwadin@live.se>
2024-09-09 12:26:29 +0200

    A

--- api/index.js
+++ api/index.js
@@ -1,13 +1,8 @@
-const fs = require('fs');
-const path = require('path');
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(require('cors')());
-
-
-const productsFilePath = path.join(__dirname, 'products.json');
 
 let products = [
     { id: 1, name: 'meeeeep', price: 1.99, description: "hejsansvensjan" },
@@ -26,30 +21,11 @@ app.get('/api/products/:id', (req, res) => {
 });
 
 app.post('/api/products', (req, res) => {
-    fs.readFile(productsFilePath, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).json({ error: 'Error reading products file' });
-        }
-
-        let products;
-        try {
-            products = JSON.parse(data);
-        } catch (parseError) {
-            return res.status(500).json({ error: 'Error parsing products data' });
-        }
-
-        const newProduct = { id: products.length + 1, ...req.body };
-        products.push(newProduct);
-
-        fs.writeFile(productsFilePath, JSON.stringify(products, null, 2), 'utf8', (writeError) => {
-            if (writeError) {
-                return res.status(500).json({ error: 'Error writing to products file' });
-            }
-
-            res.status(201).json(newProduct);
-        });
-    });
+    const newProduct = { id: products.length + 1, ...req.body };
+    products.push(newProduct);
+    res.status(201).json(newProduct);
 });
+
 app.put('/api/products/:id', (req, res) => {
     const id = +req.params.id;
     const product = findProductById(id);
